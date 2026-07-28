6bc084e171d36818b335c1c531217279620f92eb
Arnoud <arnoudja@users.noreply.github.com>
2026-07-28 20:53:14 +0200

    changed update.sh to use apt package

--- update.sh
+++ update.sh
@@ -2,20 +2,11 @@
 
 set -euo pipefail
 
-echo "### Stopping RoboMiner services ###"
-sudo systemctl stop robominer-engine robominer-web
+echo "### Building the package ###"
+rm -f ./target/debian/robominer_*_amd64.deb
+resources/scripts/build-deb.sh
 
-echo "### Buidling and deploying RoboMiner ###"
-deploy/systemd/install-robominer.sh
-
-echo "### Migrating database ###"
-sudo /opt/robominer/bin/robominer-engine migrate apply
-
-echo "### Updating databases ###"
-sudo mysql RoboMiner < resources/database/gameData.sql
-sudo mysql RoboMinerAccept < resources/database/gameData.sql
-
-echo "### Restarting RoboMiner services ###"
-sudo systemctl restart robominer-engine robominer-web
+echo "### Installing the package ###"
+sudo apt-get install --reinstall ./target/debian/robominer_*_amd64.deb
 
 echo "### Done ###"
